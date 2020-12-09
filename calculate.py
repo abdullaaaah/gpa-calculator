2f6cffbbe20afda873b023a6536d8b45c5882808
krhobeck <37283880+krhobeck@users.noreply.github.com>
2020-12-09 14:34:06 -0500

    completed convert_to_gpa function
    Done

--- calculate.py
+++ calculate.py
@@ -2,15 +2,16 @@ from typing import TextIO
 
 # SAMPLE DATA
 assessments = {
-    'a1':  {'mark': 90, 'weight': 25},
-    'a2': {'mark': 90, 'weight':25},
-    'a3': {'mark': 88, 'weight':20}
+    'a1': {'mark': 90, 'weight': 25},
+    'a2': {'mark': 90, 'weight': 25},
+    'a3': {'mark': 88, 'weight': 20}
 }
 
 # CONSTANTS
 MARK = 'mark'
 WEIGHT = 'weight'
 END_MARK = 'END'
+
 
 def calculate_mark(assessments: 'AssDict') -> float:
     """
@@ -41,6 +42,7 @@ def calculate_mark(assessments: 'AssDict') -> float:
 
     return round(current_mark / current_weight, 2)
 
+
 # TODO: add functionality to calculate exact GPA
 def convert_to_gpa(mark: float) -> float:
     """
@@ -51,10 +53,21 @@ def convert_to_gpa(mark: float) -> float:
     4.0
     >>> convert_to_gpa(84)
     3.7
+    >>> convert_to_gpa(52)
+    0.7
     """
-    pass
+    gpa_dict = {range(85, 100): 4.0, range(80, 85): 3.7,
+                range(77, 80): 3.3, range(73, 77): 3.0,
+                range(70, 73): 2.7, range(67, 70): 2.3,
+                range(63, 67): 2.0, range(60, 63): 1.7,
+                range(57, 60): 1.3, range(53, 57): 1.0,
+                range(50, 53): 0.7, range(0, 50): 0.0}
+    for i in gpa_dict:
+        if mark in i:
+            return gpa_dict[i]
 
-def calculate_GPA(assessments: 'AssDict') -> float:
+
+def calculate_gpa(assessments: 'AssDict') -> float:
     """
     Given a AssDict, <assessments> this function calculate the
     exact GPA and return
@@ -62,6 +75,7 @@ def calculate_GPA(assessments: 'AssDict') -> float:
     Round to two decimal places.
     """
     pass
+
 
 def process_assessments(f: TextIO) -> 'AllAssDict':
     """
