0b53ec973d2ec4ed305daa7f3a1eaf02de2171bd
krhobeck <37283880+krhobeck@users.noreply.github.com>
2020-12-09 15:11:22 -0500

    Changed to OOP [lots of bugs]

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
 
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def index(name="Abdullah"):
     return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
--- calculate.py
+++ calculate.py
@@ -13,79 +13,81 @@ WEIGHT = 'weight'
 END_MARK = 'END'
 
 
-def calculate_mark(gpa_assessments: 'AssDict') -> float:
-    """
-    Given a AssDict, <assessments> this function return the average of those
-    marks out of total course weight.
+class GPACalculator:
 
-    Round to two decimal places.
+    def __init__(self, assessment_dict):
+        self.assessment_dict = assessment_dict
+        self.gpa_dict = {range(85, 100): 4.0, range(80, 85): 3.7,
+                         range(77, 80): 3.3, range(73, 77): 3.0,
+                         range(70, 73): 2.7, range(67, 70): 2.3,
+                         range(63, 67): 2.0, range(60, 63): 1.7,
+                         range(57, 60): 1.3, range(53, 57): 1.0,
+                         range(50, 53): 0.7, range(0, 50): 0.0}
 
-    >>> marks = {'test': {'mark': 100, 'weight': 50}}
-    >>> calculate_mark(marks)
-    100.0
-    >>> marks2 = {'test1': {'mark': 100, 'weight': 10}, 'test2': {'mark': 50, 'weight': 20} }
-    >>> calculate_mark(marks2)
-    66.67
-    """
-    current_weight = 0
-    current_mark = 0
+    def calculate_mark(self) -> float:
+        """
+        Given a AssDict, <assessments> this function return the average of those
+        marks out of total course weight.
 
-    for i in gpa_assessments:
-        weighted_mark = (gpa_assessments[i][MARK] * gpa_assessments[i][WEIGHT])
-        current_mark += weighted_mark
-        current_weight += gpa_assessments[i][WEIGHT]
+        Round to two decimal places.
 
-    return round(current_mark / current_weight, 2)
+        >>> marks = {'test': {'mark': 100, 'weight': 50}}
+        >>> calculate_mark(marks)
+        100.0
+        >>> marks2 = {'test1': {'mark': 100, 'weight': 10}, 'test2': {'mark': 50, 'weight': 20} }
+        >>> calculate_mark(marks2)
+        66.67
+        """
+        current_weight = 0
+        current_mark = 0
 
+        for i in self.assessment_dict:
+            weighted_mark = (
+                    self.assessment_dict[i][MARK] * self.assessment_dict[i][
+                WEIGHT])
+            current_mark += weighted_mark
+            current_weight += self.assessment_dict[i][WEIGHT]
 
-# TODO: add functionality to calculate exact GPA
-def convert_to_gpa(mark: float) -> float:
-    """
-    Given a mark in two decimal places, this function
-    return the mark in GPA format.
+        return round(current_mark / current_weight, 2)
 
-    >>> convert_to_gpa(85)
-    4.0
-    >>> convert_to_gpa(84)
-    3.7
-    >>> convert_to_gpa(52)
-    0.7
-    """
-    gpa_dict = {range(85, 100): 4.0, range(80, 85): 3.7,
-                range(77, 80): 3.3, range(73, 77): 3.0,
-                range(70, 73): 2.7, range(67, 70): 2.3,
-                range(63, 67): 2.0, range(60, 63): 1.7,
-                range(57, 60): 1.3, range(53, 57): 1.0,
-                range(50, 53): 0.7, range(0, 50): 0.0}
-    for i in gpa_dict:
-        if mark in i:
-            return gpa_dict[i]
+    def convert_to_gpa(self) -> float:
+        """
+        Given a mark in two decimal places, this function
+        return the mark in GPA format.
 
+        >>> gpa_dict = {range(85, 100): 4.0, range(80, 85): 3.7, range(77, 80): 3.3, range(73, 77): 3.0, range(70, 73): 2.7, range(67, 70): 2.3, range(63, 67): 2.0, range(60, 63): 1.7, range(57, 60): 1.3, range(53, 57): 1.0, range(50, 53): 0.7, range(0, 50): 0.0}
+        >>> assessments = {'a1': {'mark': 90, 'weight': 25}, 'a2': {'mark': 90, 'weight': 25}, 'a3': {'mark': 88, 'weight': 20}}
+        4.0
+        """
 
-def calculate_gpa(assessments: 'AssDict') -> float:
-    """
-    Given a AssDict, <assessments> this function calculate the
-    exact GPA and return
+        for i in self.assessment_dict:
+            grade = i['mark']
+            if grade in self.gpa_dict:
+                return self.gpa_dict[grade]
 
-    Round to two decimal places.
-    """
+    def calculate_gpa(self, gpa_assessments: 'AssDict') -> float:
+        """
+        Given a AssDict, <assessments> this function calculate the
+        exact GPA and return
 
-    pass
+        Round to two decimal places.
+        """
 
+        pass
 
-def process_assessments(f: TextIO) -> 'AllAssDict':
-    """
-    Given an opened file, f, this function return an AssDict
-    """
-    d = {}
-    line = f.readline()
-    while line != '':
-        course_name = line.strip()
-        d[course_name] = {}
+    def process_assessments(self, f: TextIO) -> 'AllAssDict':
+        """
+        Given an opened file, f, this function return an AssDict
+        """
+        d = {}
         line = f.readline()
-        while END_MARK not in line:
-            temp = line.strip().split(",")
-            d[course_name][temp[0]] = {MARK: temp[1], WEIGHT: temp[2]}
+        while line != '':
+            course_name = line.strip()
+            d[course_name] = {}
             line = f.readline()
-        line = f.readline()
-    return d
+            while END_MARK not in line:
+                temp = line.strip().split(",")
+                d[course_name][temp[0]] = {MARK: temp[1], WEIGHT: temp[2]}
+                line = f.readline()
+            line = f.readline()
+        return d
