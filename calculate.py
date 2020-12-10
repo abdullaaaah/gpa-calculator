194c6cc8d2d4689e1254db1f3db7475fa3d83c3a
krhobeck <37283880+krhobeck@users.noreply.github.com>
2020-12-09 22:43:52 -0500

    Final calculate_gpa

--- calculate.py
+++ calculate.py
@@ -15,7 +15,7 @@ WEIGHT = 'weight'
 class GPACalculator:
 
     def __init__(self, assessment_dict: Dict):
-        self.assessment_dict = assessment_dict
+        self.class_dict = assessment_dict
         self.gpa_dict = {range(85, 100): 4.0, range(80, 85): 3.7,
                          range(77, 80): 3.3, range(73, 77): 3.0,
                          range(70, 73): 2.7, range(67, 70): 2.3,
@@ -23,7 +23,7 @@ class GPACalculator:
                          range(57, 60): 1.3, range(53, 57): 1.0,
                          range(50, 53): 0.7, range(0, 50): 0.0}
 
-    def calculate_mark(self) -> float:
+    def calculate_mark(self, class_marks: Dict) -> float:
         """
         Given a AssDict, <assessments> this function return the average of those
         marks out of total course weight.
@@ -31,25 +31,26 @@ class GPACalculator:
         Round to two decimal places.
 
         >>> marks1 = {'test': {'mark': 100, 'weight': 50}}
-        >>> G = GPACalculator(marks1)
-        >>> G.calculate_mark()
+        >>> G = GPACalculator(assessments)
+        >>> G.calculate_mark(marks1)
         100.0
         >>> marks2 = {'test1': {'mark': 100, 'weight': 10}, 'test2': {'mark': 50, 'weight': 20} }
-        >>> F = GPACalculator(marks2)
-        >>> F.calculate_mark()
+        >>> F = GPACalculator(assessments)
+        >>> F.calculate_mark(marks2)
         66.67
         """
         current_weight = 0
         current_mark = 0
 
-        for i in self.assessment_dict:
+        for assessment in class_marks:
             weighted_mark = (
-                    self.assessment_dict[i][MARK] *
-                    self.assessment_dict[i][WEIGHT])
+                    float(class_marks[assessment][MARK]) *
+                    float(class_marks[assessment][WEIGHT]))
             current_mark += weighted_mark
-            current_weight += self.assessment_dict[i][WEIGHT]
-
-        return round(current_mark / current_weight, 2)
+            current_weight += class_marks[assessment][WEIGHT]
+        if current_weight > 0:
+            return round(current_mark / current_weight, 2)
+        return 0
 
     def convert_to_gpa(self, mark: int) -> float:
         """
@@ -75,19 +76,14 @@ class GPACalculator:
 
         Round to two decimal places.
 
-        >>> G = GPACalculator(assessments)
-        >>> G.calculate_gpa()
-        4.0
+        >>> marks2 = {'test1': {'mark': 100, 'weight': 10}, 'test2': {'mark': 50, 'weight': 20} }
+        >>> F = GPACalculator(marks2)
+        >>> F.calculate_gpa()
+        2.3
         """
-        total_weight = 0
-        total_grade_weight = 0
-        for assessment in self.assessment_dict.values():
-            grade = float(assessment[MARK])
-            weight = float(assessment[WEIGHT])
-            total_weight += weight
-            total_grade_weight += grade * weight
-
-        if total_weight > 0:
-            return self.convert_to_gpa(round(total_grade_weight /
-                                             total_weight))
-        return self.convert_to_gpa(0)
+        total = 0
+        classes = len(self.class_dict)
+        for _ in range(len(self.class_dict)):
+            total += self.calculate_mark(self.class_dict)
+        average = total/classes
+        return self.convert_to_gpa(round(average))
