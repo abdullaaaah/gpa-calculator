1e1076a24c8d4314725a557b6b677c9fb1de6e6b
krhobeck <37283880+krhobeck@users.noreply.github.com>
2020-12-09 23:11:00 -0500

    app testing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask import render_template
+import calculate, processor
 
 app = Flask(__name__)
 
@@ -8,10 +9,12 @@ app = Flask(__name__)
 def index(name="Abdullah"):
     return render_template('index.html', name=name)
 
+
 @app.route('/calculate', methods=['POST'])
 def calculate():
     if request.method == 'POST':
         return request.form
+
 
 if __name__ == '__main__':
     app.run()
