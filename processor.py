37f4f5161110118ce77e2b0f60af9440bfe7b39c
Abdullah Shahid <abdullahshahid@Abdullahs-MacBook-Pro.local>
2020-12-10 00:44:08 -0500

    Fixed style error + temporarily rejecting empty forms

--- processor.py
+++ processor.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+SAMPLE_DATA = {"a1,mark":'2',"a1,weight":'5',"a2,mark":"","a2,weight":""}
 
 def dict_converter(marks: Dict) -> Dict:
     """
@@ -8,8 +9,12 @@ def dict_converter(marks: Dict) -> Dict:
     new_dict = {}
     for item in marks:
         new_item = item.split(',')
-        if new_item[0] not in new_dict:
-            new_dict[new_item[0]] = {new_item[1]: marks[item]}
-        else:
-            new_dict[new_item[0]][new_item[1]] = marks[item]
+        if marks[item] != '': #Reject empty strings for now [TEMP]
+            if new_item[0] not in new_dict:
+                new_dict[new_item[0]] = {new_item[1]: marks[item]}
+            else:
+                new_dict[new_item[0]][new_item[1]] = marks[item]
     return new_dict
+
+
+print(dict_converter(SAMPLE_DATA))
