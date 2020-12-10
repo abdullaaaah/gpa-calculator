055ad0045e29a77953772baa2d7f023712da1817
AchrafBoufer <75055522+AchrafBoufer@users.noreply.github.com>
2020-12-09 22:20:50 -0500

    dict converter tweak

--- dict converter.py
+++ dict converter.py
@@ -4,7 +4,7 @@ def dict_converter(marks:dict) -> dict:
 	for item in marks:
 		new_item = item.split(',')
 		if new_item[0] not in new_dict:
-			new_dict[new_item[0]] = {new_item[1]: marks[item]}
+			new_dict[new_item[0]] = {new_item[1]: float(marks[item])}
 		else:
-			new_dict[new_item[0]][new_item[1]] = marks[item]
+			new_dict[new_item[0]][new_item[1]] = float(marks[item])
 	return new_dict
