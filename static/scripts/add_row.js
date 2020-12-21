da0289ec0288e4318eee8451bce8ee156f4b3ded
Abdullah Shahid <abdullahshahid@Abdullahs-MacBook-Pro.local>
2020-12-21 02:40:59 -0500

    connected js generated forms

--- static/scripts/add_row.js
+++ static/scripts/add_row.js
@@ -1,24 +1,23 @@
-let START_NUM_FIELDS = 1
+let START_NUM_FIELDS = 2
 let num_fields = START_NUM_FIELDS
 
 
 function add_field()
 {
     $(".transparent").removeClass("transparent")
-    $(".trans-click").attr("onclick", null) //tryna remove this mf onclick
+    $(".trans-click").attr("onclick", null)
 
     num_fields += 1;
-
 
     $('#gpaForm').append("<div class='input-group mb-3 transparent trans-click id='" + num_fields + " onclick='add_field()'> \
                 <div class='input-group-prepend'> \
                     <span class='input-group-text'>Percent</span> \
                 </div> \
-                <input name = 'mark' type='text' class='form-control' placeholder=99 aria-describedby=basic-addon1> \
+                <input name = 'a"+ num_fields +",mark' type='text' class='form-control' placeholder=99 aria-describedby=basic-addon1> \
                   <div class='input-group-prepend'> \
                       <span class='input-group-text'>Weight</span> \
                   </div> \
-                <input name = 'weight' type='text' class='form-control' placeholder='10' aria-describedby='basic-addon1'> \
+                <input name = 'a" + num_fields + ",weight' type='text' class='form-control' placeholder='10' aria-describedby='basic-addon1'> \
                 <select class='custom-select'> \
                             <option selected>Type Of Assessment...</option> \
                             <option value='1'>Exam</option> \
@@ -36,15 +35,8 @@ function remove_field(){
     return 0
 }
 
-
+//jQuery shit here
 $(document).ready(function()
 {
-    $("#calc-gpa").click(function()
-    {
-        var dic = $("form").serializeArray();
-        $.post("/calculate", dic, function(data){
-            $('#total_percent').text(data['gpa'])
-        })
-    });
 
 });
