ebf547706e6deff816121e393d0e31bbb8b895c4
Abdullah Shahid <abdullaaaah_@outlook.com>
2020-12-25 02:18:37 -0500

    Connection

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,50 +1,21 @@
 var courses = ["CSC108", "MAT135"];
+var options = ["Assignment", "Quiz", "Tutorial Activity", "Lab", "Midterm", "Exam"];
 var marks_in_courses  = {
     CSC108: {
         "#row0": {
-          "Assignment": {
-            "100": 9
-          }
+          "Mark": 100,
+          "Weight": 9,
+          "Type": "Assignment"
         },
         "#row1": {
-          "Assignment": {
-            "100": 9
-          }
+          "Mark": 100,
+          "Weight": 9,
+          "Type": "Assignment"
         },
         "#row2": {
-          "Assignment": {
-            "93": 9
-          }
-        },
-        "#row3": {
-          "Exam": {
-            "92": 9
-          }
-        },
-        "#row4": {
-          "Exam": {
-            "100": 10
-          }
-        },
-        "#row5": {
-          "Exam": {
-            "72": 5
-          }
-        },
-        "#row6": {
-          "Exam": {
-            "100": 3
-          }
-        },
-        "#row7": {
-          "Exam": {
-            "100": 2
-          }
-        },
-        "#row8": {
-          "Exam": {
-            "80": 45
-          }
+          "Mark": 75,
+          "Weight": 25,
+          "Type": "Midterm"
         }
       }
 }
--- static/scripts/course.js
+++ static/scripts/course.js
@@ -49,6 +49,12 @@ function render_row(end=1)
     return s;
 }
 
+
+/*function render_types(first, options=options)
+{
+
+}*/
+
 function render_row_from_dict(marks)
 {
 
@@ -56,7 +62,34 @@ function render_row_from_dict(marks)
 
   for (mark in marks)
   {
-		console.log(mark)
+    s += `
+    <div class="row" id="${mark.slice(1)}">
+    <div class="col">
+        <div class="input-group mb-3" id="${mark.slice(4)}">
+            <div class="input-group-prepend">
+                <span class="input-group-text bg-light border-0 small">Percent:</span>
+            </div>
+            <input type="number" id="percent${mark.slice(4)}" value="${marks[mark].Mark}" onkeyup="update(line_chart)" class="form-control bg-light border-0 small" aria-describedby="basic-addon1">
+            <div class="input-group-prepend ">
+                <span class="input-group-text bg-light border-0 small">Weight:</span>
+            </div>
+            <input type="number" maxlength="3" value="${marks[mark].Mark}" id="weight0" onkeyup="update(line_chart)" class="form-control bg-light border-0 small" aria-describedby="basic-addon1">
+            <select value="${marks[mark].Type}" id="name${mark.slice(4)}" onchange="update(line_chart)" class="custom-select bg-light border-0 small">
+                <option value="Exam">Exam</option>
+                <option value="Midterm">Midterm</option>
+                <option value="Assignment">Assignment</option>
+                <option value="Tutorial Marks">Tutorial Marks</option>
+                <option value="Quiz">Quiz</option>
+                <option value="Lab">Lab</option>
+            </select>
+        </div>
+    </div>
+    <div class="col col-xl-2">
+        <button type="button" class="btn btn-danger" id="del_field${mark.slice(4)}" onclick="remove_field(${mark.slice(4)})">Delete</button>
+    </div>
+    </div>
+
+    `
   }
 
   return s
@@ -69,7 +102,7 @@ $(document).ready(function() {
 
     if (course_name in marks_in_courses)
     {
-
+        $("#sideBar").append(render_row_from_dict(marks_in_courses[course_name]));
     }
     else
     {
