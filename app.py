733671b38e308361e1db9074870276a0d094d42f
Abdullah Shahid <abdullaaaah_@outlook.com>
2021-01-02 00:51:39 -0500

    x

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
 from flask import render_template
-#from flask_cachebuster import CacheBuster
+from flask_cachebuster import CacheBuster
 
 app = Flask(__name__)
 
 #Cache buster
-#config = { 'extensions': ['.js', '.css', '.csv'], 'hash_size': 5 }
-#cache_buster = CacheBuster(config=config)
-#cache_buster.init_app(app)
+config = { 'extensions': ['.js', '.css', '.csv'], 'hash_size': 5 }
+cache_buster = CacheBuster(config=config)
+cache_buster.init_app(app)
 
 appName = 'SemTrack'
 
