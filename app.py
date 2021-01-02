7976cd0cdf4057ef4af58728fdf52149f90aadfa
Abdullah Shahid <abdullaaaah_@outlook.com>
2021-01-02 01:05:25 -0500

    .

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ from flask import Flask, request
 from flask import render_template
 from flask_cachebuster import CacheBuster
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 #Cache buster
 config = { 'extensions': ['.js', '.css', '.csv'], 'hash_size': 5 }
 cache_buster = CacheBuster(config=config)
-cache_buster.init_app(app)
+cache_buster.init_app(application)
 
 appName = 'SemTrack'
 
 
-@app.route("/")
+@application.route("/")
 def index(name="TrackMyCourses"):
     return render_template('index.html', name=appName, page="main")
 
-@app.route("/courses/<course_name>")
+@application.route("/courses/<course_name>")
 def course_mark_view(course_name):
     return render_template('course.html',
                            course_name=course_name,
@@ -24,4 +24,4 @@ def course_mark_view(course_name):
                            page="calc")
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
